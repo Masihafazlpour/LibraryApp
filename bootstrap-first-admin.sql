a9b6b41a5cddfc11bfe932e0826b027053e5e08e
Masihafazlpour <121658500+Masihafazlpour@users.noreply.github.com>
2026-08-05 16:40:32 +0330

    Update bootstrap-first-admin.sql

--- bootstrap-first-admin.sql
+++ bootstrap-first-admin.sql
@@ -6,7 +6,7 @@
 -- 2) UUID ساخته‌شده را به جای YOUR_AUTH_USER_UUID قرار دهید و این دستور را اجرا کنید.
 
 insert into public.profiles (id, username, full_name, role, active)
-values ('8d728f3d-ebce-485e-ad7f-fa2d7e9f4de4'::uuid, 'admin', 'مدیر سامانه', 'admin', true)
+values ('6b27eee9-118a-474a-b3ba-c0749f12ef38'::uuid, 'admin', 'مدیر سامانه', 'admin', true)
 on conflict (id) do update set
   username = excluded.username,
   full_name = excluded.full_name,
